704ca3120279cc0c91156e3974f368aa15ac6011
ShwetaBhoite <60438640+ShwetaBhoite@users.noreply.github.com>
2020-06-11 20:58:37 +0530

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,63 +1,99 @@
-async function News() {
-const res = await fetch(
-    `http://newsapi.org/v2/top-headlines?country=in&apiKey=725e179c5acb4baeb253f3b58519fce9`
+import "../styles/index.scss";
+
+//<start> code to search for news
+var inputTxt = document.getElementById("search");
+inputTxt.addEventListener("keypress", searchValue);
+function searchValue(e) {
+  var searchText = document.getElementById("search").value;
+
+  if (e.which == 13) {
+    if (searchText != "") {
+      var searchUrl = `everything?q=${searchText}`;
+      beforeLoad();
+      fetchNews(searchUrl);
+    } else {
+      beforeLoad();
+      fetchNews("top-headlines?country=in"); //by default it fetch news related to india
+    }
+  }
+}
+//<end>
+
+//function to toggle
+document.getElementById("toggler").addEventListener("click", toogleColor);
+function toogleColor() {
+  var bodyColor = document.body;
+  bodyColor.classList.toggle("dark-mode");
+
+  var btnText = document.getElementById("toggler");
+  if (btnText.innerHTML === "Dark Mode") {
+    btnText.innerHTML = "Light Mode";
+  } else {
+    btnText.innerHTML = "Dark Mode";
+  }
+}
+
+// function to fetch news list
+async function fetchNews(searchUrl) {
+  const res = await fetch(
+    `https://newsapi.org/v2/${searchUrl}&apiKey=725e179c5acb4baeb253f3b58519fce9`
   );
   const data = await res.json();
 
   if (data.totalResults > 0) {
     var output = "";
-    data.articles.forEach((art) => {
-    output += `<li class="article">
-                        <img class="article-img" src="${art.urlToImage}">
-                        <h2 class="article-title">${art.title}</h2>
-                        <p class="article-description">${art.description}</p>
-                        <span class="article-author">${art.author}</span>
-                        <a class="article-link" href="${art.url}" target="_blank"></a>
-                    </li>
-                `;
-    document.getElementById("news-articles").innerHTML = output;
+    output += '<ul id="news-articles">';
+    //array to fetch elements
+    data.articles.forEach((i) => {
+      output += `<li class="article">
+                            <img src=${i.urlToImage} alt=${i.source.name} style="width:100%;margin-top:5px;" class="article-img">
+                            
+                                <h2 class="article-title"><b>${i.title}</b></h2> 
+                                <p class="article-description">${i.description}</p> 
+                                <span class="article-author">`;
+      if (i.author != null) {
+        output += `- ${i.author}</span>`;
+      } else {
+        output += `-N.A</span>`;
+      }
+
+      output += `<br> <a href=${i.url} class="article-link" target='_blank'><em>Read More At: ${i.source.name}</em></a>
+            
+                    </li>`;
     });
-  } 
+    output += "</ul>";
+
+    document.getElementById("news-section").innerHTML = output;
+  } else if (data.totalResults === 0) {
+    var invalidData = document.getElementById("news-section");
+    invalidData.innerHTML =
+      "<h3>No article was found based on the search.</h3>";
+    invalidData.style.color = "red";
+    invalidData.classList.add("not-found");
+  }
+}
+fetchNews("top-headlines?country=in"); //by default it fetch news related to india
+
+//Take to top functionality
+
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
 
-News();
-
-
-$(document).ready(function() {
-    $('#search').keypress(() => {
-        let searchField = $("#search").val();
-        const url2 = `https://newsapi.org/v2/everything?q=${searchField}&apiKey=f918314957e941498ef9c737d29e168e';
-
-        if (searchField !== "") {
-            $.ajax({
-                url: url2,
-                method: "GET",
-                dataType: "json",
-
-                success: function(news) {
-                    let output2 = "";
-                    let articals = news.articles;
-
-                    for (var art of articals) {
-                        console.log(articals);
-                        //const art = 
-                        output2 += `
-                <li class="article">
-                    <img class="article-img" src="${art.urlToImage}">
-                    <h2 class="article-title">${art.title}</h2>
-                    <p class="article-description">${art.description}</p>
-                    <span class="article-author">${art.author}</span>
-                    <a class="article-link" href="${art.url}" target="_blank"></a>
-                </li>
-                `;
-                    }
-                    if (output2 !== "") {
-                        document.getElementById("news-articles").innerHTML = output2;
-                    } else {
-                        $("#news-articles").html("<h1 class="not-found">No article was found based on the search.</h1>");
-                    }
-                }
-            });
-        }
-    });
-});
+document.getElementById("myBtn").addEventListener("click", topFunction);
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
